1d0ba9f3175e94b0ec109803235d560042d846f4
Jbordhen <24916865+Jbordhen@users.noreply.github.com>
2021-07-15 04:24:29 +0600

    Unauthorized page.

---
+++ client/src/screens/Unauthorized.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Unauthorized = () => {
+  return (
+    <div className='text-light h1'>
+      Sorry, You are not authorized to see this page.
+    </div>
+  )
+}
+
+export default Unauthorized
